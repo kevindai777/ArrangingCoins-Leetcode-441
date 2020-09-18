8c8ebf9cd6f2d3398d44a9d91fe93a149486f306
Kevin Dai <kevin.dai.2451@gmail.com>
2020-09-18 15:13:31 -0400

    added another java solution

--- arrangingcoins.java
+++ arrangingcoins.java
@@ -13,3 +13,28 @@ class Solution {
         return count;
     }
 }
+
+
+//Alternate Java Solution w/ Binary Search
+
+class Solution {
+    public int arrangeCoins(int n) {
+        long low = 0;
+        long high = n;
+        
+        while (low <= high) {
+            long k = (low + high) / 2;
+            long curr = k * (k + 1) / 2;
+            
+            if (n == curr) {
+                return (int) k;
+            } else if (n < curr) {
+                high = k - 1;
+            } else {
+                low = k + 1;
+            }
+        }
+        
+        return (int) high;
+    }
+}
